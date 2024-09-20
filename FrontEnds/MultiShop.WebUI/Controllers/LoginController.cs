dd197f583e06d11729b06bf9db4cf00dcb1db606
Hamza <hamza.gunduuz@gmail.com>
2024-09-20 20:27:02 +0300

    SigIn User Data

--- FrontEnds/MultiShop.WebUI/Controllers/LoginController.cs
+++ FrontEnds/MultiShop.WebUI/Controllers/LoginController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
 using MultiShop.WebUI.Models;
-using MultiShop.WebUI.Services;
+using MultiShop.WebUI.Services.Interfaces;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -15,10 +15,13 @@ namespace MultiShop.WebUI.Controllers
     {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly ILoginService _loginService;
-        public LoginController(IHttpClientFactory httpClientFactory, ILoginService loginService)
+        private readonly IIdentityService _identityService;
+
+        public LoginController(IHttpClientFactory httpClientFactory, ILoginService loginService, IIdentityService identityService)
         {
             _httpClientFactory = httpClientFactory;
             _loginService = loginService;
+            _identityService = identityService;
         }
 
         [HttpGet]
@@ -66,9 +69,12 @@ namespace MultiShop.WebUI.Controllers
             return View();
         }
 
-        public IActionResult Deneme1()
+        public async Task<IActionResult> Deneme1(SignInDto signInDto)
         {
-            return View();
+            signInDto.Username = "Cilek61";
+            signInDto.Password = "Hamza-123";
+            await _identityService.SignIn(signInDto);
+            return RedirectToAction("Index","User");
         }
     }
 }
